cbdd2c22bbf5516b87fd92250e7c86075feaf435
Adam J. Kunen <kunen1@llnl.gov>
2017-06-06 11:50:51 -0700

    Added clang host config

---
+++ host-configs/blueos_3_ppc64le_ib/nvcc-clang.cmake
@@ -0,0 +1,40 @@
+##
+## Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-689114
+##
+## All rights reserved.
+##
+## For release details and restrictions, please see RAJA/LICENSE.
+##
+
+set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
+
+set(CMAKE_C_COMPILER "/usr/tcetmp/bin/mpiclang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tcetmp/bin/mpiclang++" CACHE PATH "")
+
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -O3" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Werror" CACHE STRING "")
+
+if(CMAKE_BUILD_TYPE MATCHES Release)
+  set(RAJA_NVCC_FLAGS -O2; -restrict; -arch compute_35; -std c++11; --expt-extended-lambda; -ccbin; ${CMAKE_CXX_COMPILER} CACHE LIST "")
+elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+  set(RAJA_NVCC_FLAGS -g; -G; -O2; -restrict; -arch compute_35; -std c++11; --expt-extended-lambda; -ccbin ${CMAKE_CXX_COMPILER} CACHE LIST "")
+elseif(CMAKE_BUILD_TYPE MATCHES Debug)
+  set(RAJA_NVCC_FLAGS -g; -G; -O0; -restrict; -arch compute_35; -std c++11; --expt-extended-lambda; -ccbin ${CMAKE_CXX_COMPILER} CACHE LIST "")
+endif()
+
+
+set(RAJA_ENABLE_CUDA On CACHE BOOL "")
+set(RAJA_ENABLE_OPENMP On CACHE BOOL "")
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tcetmp/packages/cuda" CACHE BOOL "")
+
+set(RAJA_RANGE_ALIGN 4 CACHE INT "")
+set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
+set(RAJA_DATA_ALIGN 64 CACHE INT "")
+set(RAJA_COHERENCE_BLOCK_SIZE 64 CACHE INT "")
+
+set(RAJA_HOST_CONFIG_LOADED On CACHE Bool "")
