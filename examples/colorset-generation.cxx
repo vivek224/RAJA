ffb3c2cd2c9358589109bda485c6b1923b207084
William Killian <william.killian@gmail.com>
2017-07-24 14:15:08 -0700

    Expand test for colorset

--- examples/colorset-generation.cxx
+++ examples/colorset-generation.cxx
@@ -18,11 +18,53 @@ int main()
       putchar('\n');
     }
   }
-  puts("Iteration for extents={{5, 5}}, lower={{1, 1}}, upper={{4, 4}}");
+  puts("Iteration for extents={{6, 6}}, lower={{1, 1}}, upper={{5, 5}}");
   {
     auto Result = RAJA::make_colorset(std::array<RAJA::Index_type, 2>{{6, 6}},
                                 std::array<RAJA::Index_type, 2>{{1, 1}},
                                 std::array<RAJA::Index_type, 2>{{5, 5}});
+    for (auto i : Result) {
+      auto seg = Result.getSegment<RAJA::TypedListSegment<RAJA::Index_type>>(i);
+      printf("Segment %lu:\n", i);
+      RAJA::forall<RAJA::seq_exec>(seg, [=](RAJA::Index_type idx) {
+        printf("%lu ", idx);
+      });
+      putchar('\n');
+    }
+  }
+  puts("Iteration for extents={{5}}, lower={{1}}, upper={{4}}");
+  {
+    auto Result = RAJA::make_colorset(std::array<RAJA::Index_type, 1>{{5}},
+                                      std::array<RAJA::Index_type, 1>{{1}},
+                                      std::array<RAJA::Index_type, 1>{{4}});
+    for (auto i : Result) {
+      auto seg = Result.getSegment<RAJA::TypedListSegment<RAJA::Index_type>>(i);
+      printf("Segment %lu:\n", i);
+      RAJA::forall<RAJA::seq_exec>(seg, [=](RAJA::Index_type idx) {
+        printf("%lu ", idx);
+      });
+      putchar('\n');
+    }
+  }
+  puts("Iteration for extents={{5, 5}}, lower={{1, 1}}, upper={{4, 4}}");
+  {
+    auto Result = RAJA::make_colorset(std::array<RAJA::Index_type, 2>{{5, 5}},
+                                      std::array<RAJA::Index_type, 2>{{1, 1}},
+                                      std::array<RAJA::Index_type, 2>{{4, 4}});
+    for (auto i : Result) {
+      auto seg = Result.getSegment<RAJA::TypedListSegment<RAJA::Index_type>>(i);
+      printf("Segment %lu:\n", i);
+      RAJA::forall<RAJA::seq_exec>(seg, [=](RAJA::Index_type idx) {
+        printf("%lu ", idx);
+      });
+      putchar('\n');
+    }
+  }
+  puts("Iteration for extents={{5, 5, 5}}, lower={{1, 1, 1}}, upper={{4, 4, 4}}");
+  {
+    auto Result = RAJA::make_colorset(std::array<RAJA::Index_type, 3>{{5, 5, 5}},
+                                      std::array<RAJA::Index_type, 3>{{1, 1, 1}},
+                                      std::array<RAJA::Index_type, 3>{{4, 4, 4}});
     for (auto i : Result) {
       auto seg = Result.getSegment<RAJA::TypedListSegment<RAJA::Index_type>>(i);
       printf("Segment %lu:\n", i);
