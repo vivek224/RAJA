461bbedc23bb9891049108f191bdcb1a8bd39545
William Killian <william.killian@gmail.com>
2017-07-24 14:42:44 -0700

    Add documentation for ColorSetBuilder

--- include/RAJA/util/ColorSetBuilder.hpp
+++ include/RAJA/util/ColorSetBuilder.hpp
@@ -157,6 +157,18 @@ struct ColorSet_BuildList<Vals<Curr, Rest...>, Vals<All...>, IndexType> {
   using Next = ColorSet_BuildList<Vals<Rest...>, Vals<All...>, IndexType>;
   using Buffer = IndexType *;
 
+  /// generate loop where we want to iterate from lower + dim < upper by 2
+  /*!
+   * \param[out] buffer location to write output indicies
+   * \param[in] offsets index offsets for each dimension -- makes calculating
+   * absolute index as easy as a dot product
+   * \param[in] lower lower bound ND-point for index generation
+   * \param[in] upper upper bound ND-point for index generation
+   * \param[in] dim current starting dimension which will be an ND-point in the
+   * range of [0,1] for each dimension
+   * \param[in] ids list of indicies. once all indicies have been added, the
+   * loop body will be called
+   */
   template <typename Array, typename... Idx>
   static inline void generate(Buffer &buffer,
                               const Array &offsets,
@@ -182,6 +194,17 @@ struct ColorSet_BuildList<Vals<>, Vals<All...>, IndexType> {
   using Next = ColorSet_BuildList<Vals<All...>, Vals<All...>, IndexType>;
   using Buffer = IndexType *;
 
+  /// emit loop body where we write to the buffer and increment the buffer write
+  /// pointer
+  /*!
+   * \param[out] buffer to location to write; it is advanced once written to
+   * \param[in] offsets index offsets for each dimension -- makes calculating
+   * absolute index as easy as a dot product
+   * \param lower unused
+   * \param upper unused
+   * \param dim unused
+   * \param[in] ids list of indicies for current ND-point
+   */
   template <typename Array, typename... Idx>
   static inline void generate(Buffer &buffer,
                               const Array &offsets,
@@ -194,6 +217,25 @@ struct ColorSet_BuildList<Vals<>, Vals<All...>, IndexType> {
   }
 };
 
+/*!
+ * \brief This class is used to generate an ND-nested loop for colorset
+ * generation
+ *
+ * The basic premise is in order to correctly do colorset generation, we need to
+ * determine the indicies that can be operated on concurrently.
+ *
+ * For a 1D structure, we will have two sets: one where we handle the odd
+ * elements while the other will handle all of the even elements
+ *
+ * For a 2D structure, we will have four sets: the even and odd sets for each
+ * of the two dimensions.
+ *
+ * For an ND structure, we will have pow(2,N) sets: the total possible sets
+ * generated from even and odd start indexing in each dimension
+ *
+ * This class generalizes the emmision of the N nested loops required for
+ * indexing.
+ */
 template <typename, typename, typename IndexType>
 struct ColorSet_EachColor;
 
@@ -206,6 +248,19 @@ struct ColorSet_EachColor<Vals<Curr, Rest...>, Vals<All...>, IndexType> {
   using Next = ColorSet_EachColor<Vals<Rest...>, Vals<All...>, IndexType>;
   using Buffer = IndexType *;
 
+  /// emit a loop over an even and odd (0,1) sequence to determine the current
+  /// independent segment
+  /*!
+   * \param[out] iset the index set to populate
+   * \param[out] buffer temporary buffer used for intermediate storage of
+   * indicies
+   * \param[in] offsets index offsets for each dimension -- makes calculating
+   * absolute index as easy as a dot product
+   * \param[in] lower lower bound ND-point for index generation
+   * \param[in] upper upper bound ND-point for index generation
+   * \param[in] ids current starting dimension which will be an ND-point in the
+   * range of [0,1] for each dimension visited so far
+   */
   template <typename IdxSet, typename Array, typename... Idx>
   static inline void generate(IdxSet &iset,
                               Buffer buffer,
@@ -225,6 +280,26 @@ struct ColorSet_EachColor<Vals<>, Vals<All...>, IndexType> {
   using Next = ColorSet_BuildList<Vals<All...>, Vals<All...>, IndexType>;
   using Buffer = IndexType *;
 
+  /// emit a loop over an even and odd (0,1) sequence to determine the current
+  /// independent segment
+  /*!
+   * \param[out] iset the index set to populate
+   * \param[out] buffer temporary buffer used for intermediate storage of
+   * indicies
+   * \param[in] offsets (Forwarded) index offsets for each dimension -- makes
+   * calculating absolute index as easy as a dot product
+   * \param[in] lower (Forwarded) lower bound ND-point for index generation
+   * \param[in] upper (Forwarded) upper bound ND-point for index generation
+   * \param[in] ids all points from visiting the ND space -- this determines the
+   * current color group to populate
+   *
+   * This function has a precondition that all dimensions have been visited
+   * (with a for-loop generated for each one). We invoke a function that will
+   * populate @buffer with the valid indicies starting at a base point of @ids
+   * across our ND space restricted by @lower and @upper. Finally, we will add
+   * the contents of @buffer to a new @ListSegment and insert this new segment
+   * into @iset.
+   */
   template <typename IdxSet, typename Array, typename... Idx>
   static inline void generate(IdxSet &iset,
                               Buffer buffer,
@@ -240,17 +315,23 @@ struct ColorSet_EachColor<Vals<>, Vals<All...>, IndexType> {
   }
 };
 
+/// Primitive class for temporary storage destroyed upon destructor invocation
 template <typename T>
 struct TempBuffer {
   T *data;
   constexpr TempBuffer(unsigned long N) : data(new T[N]) {}
-  ~TempBuffer()
-  {
-    delete[] data;
-  }
+  ~TempBuffer() { delete[] data; }
   constexpr operator T *() const { return data; }
 };
 
+/// Internal function used to dispatch colorset construction
+/*!
+ * \param buffer temporary storage
+ * \param[in] offsets computed offsets for each dimension for fast index
+ * calculation (single dot product)
+ * \param[in] lower lower ND-point of colorset index range
+ * \param[out] upper upper ND-point of colorset index range
+ */
 template <typename IdxSet, typename Array>
 static inline IdxSet build_colorset(typename Array::value_type *buffer,
                                     const Array &offsets,
@@ -271,9 +352,9 @@ static inline IdxSet build_colorset(typename Array::value_type *buffer,
 /*!
  *  \param[in] extents ND-range specifying the extents in each dimension
  *  \param[in] lower ND-point specifying the lower iteration space limit
- * (inclusive)
+ * (inclusive). This must be >= [0, 0, ...]
  *  \param[in] upper ND-point specifying the upper iteration space limit
- * (exclusive)
+ * (exclusive). This must be <= [extents...]
  */
 template <unsigned long N, typename IndexType>
 RAJA::StaticIndexSet<RAJA::TypedListSegment<IndexType>> make_colorset(
