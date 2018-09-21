e266d93ffc3f79e8d41530303fac1ea91cc7faaa
Vivek Kale <vivek.lkale@gmail.com>
2018-09-21 18:00:03 -0500

    Update changes with lws sched for stat/dyn staggered scheduling.

--- include/RAJA/policy/openmp/forall.hpp
+++ include/RAJA/policy/openmp/forall.hpp
@@ -53,6 +53,8 @@
 
 #include "vSched.c"
 
+#define FORALL_BEGIN(strat, s,e, start, end, tid, numThds )  loop_start_ ## strat (s,e ,&start, &end, tid, numThds);  do {
+#define FORALL_END(strat, start, end, tid)  } while( loop_next_ ## strat (&start, &end, tid));
 
 namespace RAJA
 {
@@ -77,9 +79,7 @@ RAJA_INLINE void forall_impl(const omp_parallel_exec<InnerPolicy>&,
       using RAJA::internal::thread_privatize;
       auto body = thread_privatize(loop_body);
       forall_impl(InnerPolicy{}, iter, body.get_priv());
-
     });
-
 }
 
 ///
@@ -124,10 +124,16 @@ RAJA_INLINE void forall_impl(const omp_lws<&,
                              Func&& loop_body)
 {
   RAJA_EXTRACT_BED_IT(iter);
-  for (decltype(distance_it) i = 0; i < distance_it; ++i) {
+  int startInd, endInd;
+  int threadNum = omp_get_thread_num();
+  int numThreads = omp_get_num_threads();
+  
+  FORALL_BEGIN(statdynstaggered, 0, distance_it, startInd, endInd, threadNum, numThreads)
+  for (decltype(distance_it) i = startInd; i < endInd; ++i) {
     loop_body(begin_it[i]);
   }
-  
+  FORALL_END(statdynstaggered, startInd, endInd, threadNum)
+      /* thread reduction */
 }
   
 ///
@@ -195,7 +201,7 @@ RAJA_INLINE void forall(
   for (int isi = 0; isi < num_seg; ++isi) {
     IndexSetSegInfo* seg_info = ncis.getSegmentInfo(isi);
     DepGraphNode* task = seg_info->getDepGraphNode();
-
+    
     task->wait();
 
     executeRangeList_forall<SEG_EXEC_POLICY_T>(seg_info, loop_body);
