511bc3a237005e6ca03b76daa2aa3154e5e052c4
Vivek Kale <vivek.lkale@gmail.com>
2018-10-03 15:51:16 -0500

    Updating with forall.hpp

--- include/RAJA/policy/lws/openmp/forall.hpp
+++ include/RAJA/policy/lws/openmp/forall.hpp
@@ -111,6 +111,28 @@ RAJA_INLINE void forall_impl(const omp_for_exec&,
   }
 }
 
+
+  ///              
+  /// OpenMP parallel lws policy implementation.
+  ///
+
+  template <typename Iterable, typename Func>
+  RAJA_INLINE void forall_impl(const omp_lws&,
+			       Iterable&& iter,
+			       Func&& loop_body)
+  {
+    RAJA_EXTRACT_BED_IT(iter);
+    int startInd, endInd;
+    int threadNum = omp_get_thread_num();
+    int numThreads = omp_get_num_threads();
+    FORALL_BEGIN(statdynstaggered, 0, distance_it, startInd, endInd, threadNum, numThreads)
+      for (decltype(distance_it) i = startInd; i < endInd; ++i) {
+	loop_body(begin_it[i]);
+      }
+    FORALL_END(statdynstaggered, startInd, endInd, threadNum)
+
+      }
+
 ///
 /// OpenMP parallel for static policy implementation
 ///
